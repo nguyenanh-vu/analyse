1499fc50f69dc16451fb0c487d0720926922600b
NguyenAnhVUNancy <85106168+NguyenAnhVUNancy@users.noreply.github.com>
2022-10-28 02:33:59 -0400

    Update NotEnoughArgumentException.java

--- src/main/java/analyse/exceptions/NotEnoughArgumentException.java
+++ src/main/java/analyse/exceptions/NotEnoughArgumentException.java
@@ -4,7 +4,7 @@ public class NotEnoughArgumentException extends Exception {
 	private static final long serialVersionUID = 913820398760353023L;
 
 	public NotEnoughArgumentException(String s, int expected, int given) {
-		super(String.format("%s : Not enough arguments, %s expected, % given", 
+		super(String.format("%s : Not enough arguments, %s expected, %s given", 
 				s, String.valueOf(expected), String.valueOf(given)));
 	} 
 }
